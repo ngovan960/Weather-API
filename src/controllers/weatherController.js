b63342cc96abe2232b725222706ae77d13b4c34f
Hieu <ngovan960@gmail.com>
2025-11-05 13:47:48 +0700

    success

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,10 +1,32 @@
 import axios from "axios";
 
 export const getWeather = async (req, res) => {
+  const redisClient = req.redisClient;
+  const cityCode = req.params.cityCode;
+
+  const cacheKey = "weather:" + cityCode;
+  const time = 60 * 60 * 12;
   try {
-    const cityCode = req.params.cityCode;
-    console.log(cityCode);
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    //Kiem tra cache trong redis
+    const cachedData = await redisClient.get(cacheKey);
+    if (cachedData) {
+      console.log("Data from Redis Cache");
+      return res.json(JSON.parse(cachedData));
+    }
+    console.log("Call API Weather");
+    const apiKey = process.env.API_KEY;
+    const baseUrl = process.env.BASE_URL;
+    const apiUrl = `${baseUrl}${cityCode}?unitGroup=metric&key=${apiKey}`;
+    const response = await axios.get(apiUrl);
+    const data = response.data;
+
+    // Luu vao redis
+
+    await redisClient.setEx(cacheKey, time, JSON.stringify(data));
+
+    res.json(data);
+  } catch (err) {
+    console.error("Error fetching weather data: ", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import weatherRoute from "./routes/weather/weatherRoute.js";
+import { createClient } from "redis";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+const redisClient = createClient({
+  url: `redis://${process.env.REDIS_URL}:${process.env.REDIS_PORT}`,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("Connected to Redis"))
+  .catch((err) => console.log("Redis connection error: ", err));
+
+app.use((req, res, next) => {
+  req.redisClient = redisClient;
+  next();
+});
 app.use("/weather", weatherRoute);
 
 app.get("/", (req, res) => {
